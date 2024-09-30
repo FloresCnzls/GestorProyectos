23b75ae601a54bade5eb0743aa9aa8cf12fe62a2
FloresCnzls <85141586+FloresCnzls@users.noreply.github.com>
2024-09-29 22:48:59 -0600

    cambios en login

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation'; // Usa 'next/navigation' en lugar de 'next/router'
+import { useRouter } from 'next/navigation'; 
 import styles from "./page.module.css";
 
-export default function Login() {
-  console.log('Componente Login renderizado'); // Verificar si el componente está renderizando
+export default function Login() { 
 
   const [nombre_usuario, setnombre_usuario] = useState('');
   const [contrasena, setcontrasena] = useState('');
@@ -15,11 +14,6 @@ export default function Login() {
   const handleSubmit = async (e) => {
     
     e.preventDefault();
-
-    // Verifica si esta línea aparece en la consola
-    console.log('Formulario enviado');
-    
-
     try {
       const response = await fetch('/api/login', {
         method: 'POST',
@@ -31,22 +25,20 @@ export default function Login() {
           contrasena: contrasena,
         }),
       });
-          // Verifica el estado de la respuesta de la API
-          console.log('Estado de la respuesta:', response.status);
+         
 
       if (response.ok) {
         const data = await response.json();
-        console.log('Conexión exitosa:', data);  // Mostrar respuesta exitosa en la consola
-        // Guarda el token en localStorage
+       
         localStorage.setItem('token', data.token);
-        router.push('/home'); // Redirige al dashboard después del login
+        router.push('/home'); 
       } else {
         const errorData = await response.json();
-        console.log('Error en la respuesta de la API:', errorData);  // Mostrar error de la API
+       
         setError(errorData.message || 'Credenciales incorrectas');
       }
     } catch (err) {
-      console.log('Error de conexión:', err);  // Mostrar cualquier error de conexión
+      
       setError('Hubo un problema con la conexión');
     }
   };
@@ -54,7 +46,6 @@ export default function Login() {
     <div className={styles.container}>
       <div className={styles.loginBox}>
         <h1 className={styles.title}>Iniciar Sesión</h1>
-         {/* Mostrar mensaje de error si existe */}
          {error && <p className={styles.errorMessage}>{error}</p>}
         <form className={styles.form}  onSubmit={handleSubmit}>
           <div className={styles.inputGroup}>
