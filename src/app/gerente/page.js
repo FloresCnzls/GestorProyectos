30f485d7813a172b94f178a4c93071a75ad9802d
Carlos Campos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2024-10-04 21:23:53 -0600

    gerente

--- src/app/gerente/page.js
+++ src/app/gerente/page.js
@@ -1,11 +1,34 @@
-'use client';
-import React from 'react';
-import ProjectForm from './ProjectForm';
+"use client"
 
- export default function  App() {
-  return (
-    <div>
-      <ProjectForm />
-    </div>
-  );
-};
+import React, {useState} from "react";
+import GerenteMenu from "./menuGerente/gerenteMenu";
+import GestionProyectos from "./gestionProyectos/gestionProyectos";
+import GestionTareas from "./gestionTareas/gestionTareas";
+
+export default function Home(){
+    const [activeModule, setActiveModule] = useState("proyectos");
+
+    // Función para cambiar el módulo según el menú seleccionado
+    const handleMenuClick = (module) => {
+        if (typeof module !== "string") {
+            console.error("El parámetro del menú no es válido:", module);
+            return;
+          }
+          setActiveModule(module);
+    };
+
+    return (
+        <div>
+          {/* Pasa la función para manejar el clic en el menú como prop */}
+          <GerenteMenu handleMenuClick={handleMenuClick} />
+    
+          {/* Renderiza el módulo basado en el estado */}
+          <main>
+            {activeModule === "proyectos" && <GestionProyectos />}
+            {activeModule === "tareas" && <GestionTareas />}
+            {activeModule === "actus" && <AboutModule />}
+            {activeModule === "contactos" && <ContactModule />}
+          </main>
+        </div>
+      );
+}
