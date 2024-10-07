f0f1334c1ce1c8a99afd6f563e64df05aa9fa4a2
Alex-Cruz6 <josealexcruz6@gmail.com>
2024-10-07 00:13:21 -0600

    modification to make vercel work

--- src/app/gerente/menuGerente/page.js
+++ src/app/gerente/menuGerente/page.js
@@ -1,34 +1,35 @@
-"use client"
+"use client";
 
 import React from 'react';
 import styles from './Menu.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
-const GerenteMenu = ({handleMenuClick}) => {
-    return(
+const GerenteMenu = ({ handleMenuClick }) => {
+    return (
         <header className={styles.header}>
-       <Image
-        src="/logo.png"  
-        alt="Logo de la aplicación"
-        width={150}
-        height={50}
-        priority
-      />
-          <nav className={styles.nav}>
-          <a href="#" onClick={() => handleMenuClick("proyectos")}>
-            Gestion de Proyectos
-          </a>
-          <a href="#" onClick={() => handleMenuClick("tareas")}>
-            Asignar Tareas
-          </a>
-          <a href="#" onClick={() => handleMenuClick("actus")}>
-            Ver actualizaciones
-          </a>
-          <a href="#" onClick={() => handleMenuClick("contacto")}>
-            Contacto
-          </a>
-        </nav>
-      </header>
+            <Image
+                src="/logo.png"
+                alt="Logo de la aplicación"
+                width={150}
+                height={50}
+                priority
+            />
+            <nav className={styles.nav}>
+                <Link href="#" onClick={() => handleMenuClick("proyectos")}>
+                    Gestion de Proyectos
+                </Link>
+                <Link href="#" onClick={() => handleMenuClick("tareas")}>
+                    Asignar Tareas
+                </Link>
+                <Link href="#" onClick={() => handleMenuClick("actus")}>
+                    Ver actualizaciones
+                </Link>
+                <Link href="#" onClick={() => handleMenuClick("contacto")}>
+                    Contacto
+                </Link>
+            </nav>
+        </header>
     );
 };
 
--- src/app/gerente/page.js
+++ src/app/gerente/page.js
@@ -1,34 +1,31 @@
-"use client"
+"use client";
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import GerenteMenu from "./menuGerente/page";
 import GestionProyectos from "./gestionProyectos/page";
 import GestionTareas from "./gestionTareas/page";
 
-export default function Home(){
+export default function Home() {
     const [activeModule, setActiveModule] = useState("proyectos");
 
-    // Función para cambiar el módulo según el menú seleccionado
     const handleMenuClick = (module) => {
         if (typeof module !== "string") {
             console.error("El parámetro del menú no es válido:", module);
             return;
-          }
-          setActiveModule(module);
+        }
+        setActiveModule(module);
     };
 
     return (
         <div>
-          {/* Pasa la función para manejar el clic en el menú como prop */}
-          <GerenteMenu handleMenuClick={handleMenuClick} />
-    
-          {/* Renderiza el módulo basado en el estado */}
-          <main>
-            {activeModule === "proyectos" && <GestionProyectos />}
-            {activeModule === "tareas" && <GestionTareas />}
-            {activeModule === "actus" && <AboutModule />}
-            {activeModule === "contactos" && <ContactModule />}
-          </main>
+            <GerenteMenu handleMenuClick={handleMenuClick} />
+
+            <main>
+                {activeModule === "proyectos" && <GestionProyectos />}
+                {activeModule === "tareas" && <GestionTareas />}
+                {activeModule === "actus" && <h2>Modulo de Actualizaciones</h2>}
+                {activeModule === "contacto" && <h2>Modulo de Contacto</h2>}
+            </main>
         </div>
-      );
+    );
 }
